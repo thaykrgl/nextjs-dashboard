64b89634903c4fc63303adf2c204389bd1a429da
Taha <tahaaykiroglu99@gmail.com>
2023-11-20 15:34:07 +0300

    remove build error

--- nextjs-dashboard/app/ui/invoices/table.tsx
+++ nextjs-dashboard/app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt="invoice image"
                       />
                       <p>{invoice.name}</p>
                     </div>
