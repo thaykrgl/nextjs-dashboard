6d0a01ba53a3a9178d85e7183a2c6f6ccc6e2351
Taha <tahaaykiroglu99@gmail.com>
2023-11-27 07:58:34 +0300

    complete chapter 13

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -66,6 +66,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
